de46841e767262aba2b91bac459d53f602d24ca8
Andreas Petrovic <andreas.petrovic@petrobros.net>
2014-07-24 17:26:43 +0200

    Added fitconfig loader

---
+++ src/utils/fitconfigloader/fitconfigloader.cpp
@@ -0,0 +1,5 @@
+#include "fitconfigloader.h"
+
+FitConfigLoader::FitConfigLoader()
+{
+}
---
+++ src/utils/fitconfigloader/fitconfigloader.h
@@ -0,0 +1,10 @@
+#ifndef FITCONFIGLOADER_H
+#define FITCONFIGLOADER_H
+
+class FitConfigLoader
+{
+public:
+    FitConfigLoader();
+};
+
+#endif // FITCONFIGLOADER_H
